01a9b09a6d308b2464e8592ab27f43b902164774
kamaxacademy <kmaxacademy28@gmail.com>
2021-02-25 13:28:05 -0500

    added users function in database and controllers

--- openlibrary-desktop/server/controllers/userController.js
+++ openlibrary-desktop/server/controllers/userController.js
@@ -1,7 +1,40 @@
+import { v4 as uuid } from 'uuid';
+
+let users = [];
 
 
 //Get all Users in Database
 
 export const getAllUsers = (req, res)=>{
-    res.send("This is to get all users")
+    console.log(`Users in the database: ${users}`);
+
+    res.send(users);
 }
+
+export const createUser = (req, res) => { 
+    const user = req.body;
+
+    users.push({...user, id: uuid()});
+    
+    res.send(`User [${user.username}] added to the database.`);
+}
+
+export const getUser = (req, res) => {
+    res.send(req.params.id)
+};
+
+
+export const deleteUser = (req, res) => { 
+    res.send(`user with id ${req.params.id} has been deleted`);
+    
+    users = users.filter((user) => user.id !== req.params.id);
+};
+
+export const updateUser =  (req,res) => {
+    const user = users.find((user) => user.id === req.params.id);
+    
+    user.username = req.body.username;
+    user.age = req.body.age;
+
+    res.send(`username has been updated to ${req.body.username}.age has been updated to ${req.body.age}`)
+};
--- openlibrary-desktop/server/routes/users.js
+++ openlibrary-desktop/server/routes/users.js
@@ -2,15 +2,27 @@
 import express from "express"
 
 // Import users-controller
-import {getAllUsers} from '../controllers/userController.js'
+import {getAllUsers, createUser, getUser, deleteUser, updateUser} from '../controllers/userController.js'
 
 // Create router
 const router = express.Router()
 
+
+
 // Add route for GET request to retrieve all users
 // In server.js, users route is specified as '/users'
 // this means that '/all' translates to '/users/all'
-router.get('/all', getAllUsers)
+router.get('/', getAllUsers)
 
+
+router.post('/', createUser)
+
+
+router.get('/:id',getUser)
+
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
 
 export default router;
